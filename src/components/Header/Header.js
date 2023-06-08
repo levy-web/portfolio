9d5b9dc63cafdd8e18f57622223b22878e4ed5de
levy <lnjiru43@gmail.com>
2023-06-08 11:05:41 +0300

    navs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './Heade
 const Header = () =>  (
   <Container>
     <Div1>
-      <Link href="#hero">
+      <Link href="/">
         <a style={{display: "flex", alignItems: "center", color: "white", }}>
           <Span>Portfolio</Span>
         </a>
